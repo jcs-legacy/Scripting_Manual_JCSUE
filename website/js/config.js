20512e96630602156856a4ea900c19bed52f431b
jenchieh <jcs090218@gmail.com>
2018-10-26 20:58:01 +0800

    Add client core.

---
+++ website/js/config.js
@@ -0,0 +1,42 @@
+/**
+ * $File: config.js $
+ * $Date: 2018-09-28 22:33:54 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2018 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+
+// Manual config.
+const manual_name = "JCSUE";
+const version_manual = "0.0.1";
+
+// Copyright text to show on the website.
+const copyright_text = "Copyright © 2018 JCSUE Group. Built: 2018-10-26.";
+
+// Default content page.
+const intro_content = 'intro';
+// Search page.
+const search_content = 'search';
+
+// Homepage Link
+const homepage_url = "https://github.com/jcs090218/API-Reference-Template";  // The actual link to your homepage.
+const homepage_text = "api-ref-tem.github";  // Text to show on the browser
+
+
+// Content extension to load.
+//
+// NOTE(jenchieh): have to be the same with 'server' side's
+// config file.
+//
+// SEE : confid.js in the project root directory.
+const content_extension = '.html';
+
+// Search Input config
+const si_manual_placeholder = "Search manual...";
+const si_api_placeholder = "Search scripting...";
+
+const si_input_size = 17;  // character unit.
