12bb56460e548ff993d4b96f7d4ebd40530b9f63
jenchieh <jcs090218@gmail.com>
2018-10-26 21:01:10 +0800

    Add util scripts.

---
+++ scripts/start_forever.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# ========================================================================
+# $File: start_forever.sh $
+# $Date: 2018-10-04 21:33:12 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2018 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+# Back to root directory.
+cd ..
+
+# Run the server.
+forever start main.js -o log.txt
