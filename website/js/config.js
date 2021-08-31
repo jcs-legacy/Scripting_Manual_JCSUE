0ce63d50c07de4c679f66582f5bb0fbccd32563d
JenChieh <jcs090218@gmail.com>
2021-08-31 23:28:22 +0800

    Update config

--- website/js/config.js
+++ website/js/config.js
@@ -10,7 +10,7 @@
 "use strict";
 
 /* Manual */
-const manual_name = "JCSUE";
+const manual_name = 'JCSUE';
 
 /* Theme */
 const header_color = '#2A2A2A';
@@ -19,32 +19,32 @@ const th_show      = true;       // table header
 const th_color     = '#A2F2DE';  // table header color
 
 /* Version */
-const version_title_01 = "JCSUE Version: ";
-const version_title_02 = "Unreal Version: ";
-const version_title_03 = "";
-const version_title_04 = "";
-const version_title_05 = "";
-const version_title_06 = "";
-const version_title_07 = "";
+const version_title_01 = 'JCSUE Version: ';
+const version_title_02 = 'Unreal Version: ';
+const version_title_03 = '';
+const version_title_04 = '';
+const version_title_05 = '';
+const version_title_06 = '';
+const version_title_07 = '';
 
-const version_num_01 = "0.0.1";
-const version_num_02 = "4.25.1";
-const version_num_03 = "";
-const version_num_04 = "";
-const version_num_05 = "";
-const version_num_06 = "";
-const version_num_07 = "";
+const version_num_01 = '0.0.1';
+const version_num_02 = '4.25.1';
+const version_num_03 = '';
+const version_num_04 = '';
+const version_num_05 = '';
+const version_num_06 = '';
+const version_num_07 = '';
 
 /* Copyright */
-const copyright_text = "Copyright © 2018 JCSUE Group. Built: 2018-10-26.";
+const copyright_text = 'Copyright © 2018 JCSUE Group. Built: 2018-10-26.';
 
 /* Others */
 const intro_content  = 'intro';   // Default content page.
 const search_content = 'search';  // Search page.
 
 /* Homepage Link */
-const homepage_url  = "https://github.com/jcs090218/JCSUE";  // Link to your homepage.
-const homepage_text = "JCSUE.github";                        // Text represent your homepage.
+const homepage_url  = 'https://github.com/jcs090218/JCSUE';  // Link to your homepage.
+const homepage_text = 'JCSUE.github';                        // Text represent your homepage.
 
 
 // Content extension to load.
@@ -55,7 +55,7 @@ const homepage_text = "JCSUE.github";                        // Text represent y
 const content_extension = '.md';
 
 /* Search Input Text */
-const si_manual_placeholder = "Search manual...";
-const si_api_placeholder    = "Search scripting...";
+const si_manual_placeholder = 'Search manual...';
+const si_api_placeholder    = 'Search scripting...';
 
 const si_input_size = 17;  // character unit.
